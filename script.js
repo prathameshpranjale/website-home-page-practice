2deed7650176258e79b4aef5e059bb6ec27b7ab8
Prathamesh Pranjale <99072435+prathameshpranjale@users.noreply.github.com>
2024-08-12 21:30:25 +0530

    basic practice

---
+++ script.js
@@ -0,0 +1,10 @@
+const navbar = document.getElementById("nav-links");
+
+// java script code for toggle meue 
+function showmenu() {
+    navbar.classList.add("show");
+}
+
+function hidemenu() {
+    navbar.classList.remove("show");
+}
